d295c050fdd5b233ff7546d39e790891b94f170a
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-04-10 15:55:00 +0200

    Pozbycie się efektów ubocznych zmiany stanu obiektu

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -6,10 +6,10 @@ class TestClass {
 
     @Test
     public void testMultiplication() {
-        Dollar five= new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-        // tu jest komentarz do wyrzucenia
+            Dollar five= new Dollar(5);
+            Dollar product= five.times(2);
+            assertEquals(10, product.amount);
+            product= five.times(3);
+            assertEquals(15, product.amount);
+        }
     }
-
-}
