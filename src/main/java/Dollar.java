e5cc2366b5c1636be9e54854395bfa38bc5d1284
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-04-10 16:59:09 +0200

    Create parent class Money and refactor with subslasses Dolar and Franc

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,5 +1,4 @@
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
 
     Dollar(int amount) {
         this.amount = amount;
@@ -9,11 +8,7 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object object) {
-            Dollar dollar = (Dollar) object;
-            return amount == dollar.amount;
-    }
+
 
 }
 
